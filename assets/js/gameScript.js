9b8964141c219b62b7ae62798c0fe33e24881355
LoftusN13 <loftus.niamh@yahoo.ie>
2021-01-25 20:43:59 +0000

    functions for tile matching

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -1,7 +1,47 @@
-let cards = document.querySelectorAll(".game-tile");
+/* Declared Variables */
+const tiles = document.querySelectorAll(".game-tile");
 
-function flipCard() {
-  this.classList.toggle('flip');
+let flippedTile = false;
+let firstTile, secondTile;
+
+/* Flip Game Tile When Clicked */
+function flipTile() {
+    this.classList.add("flip");
+
+    if(!flippedTile) {
+        flippedTile = true;
+        firstTile = this;
+        return;
+    }
+
+    secondTile = this;
+    flippedTile = false;
+
+    checkForMatch();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+/* Check if Two Tiles Match */
+function checkForMatch(){
+    if(firstTile.dataset.type === secondTile.dataset.type) {
+    disableTiles();
+    return;
+    }
+
+    unflipTiles();
+}
+
+/* Disable Matched Tiles */
+function disableTiles() {
+    firstTile.removeEventListener("click", flipTile);
+    secondTile.removeEventListener("click", flipTile);
+}
+
+/* Flip Tiles Back If Not Matched */
+function unflipTiles() {
+    setTimeout(() => {
+     firstTile.classList.remove("flip");
+     secondTile.classList.remove("flip");
+   }, 1200);
+}
+
+tiles.forEach(tile => tile.addEventListener("click", flipTile));
